c24c3aaddbe772c5c033a6af564c3368381eb375
J-P Nurmi <jpnurmi@gmail.com>
2020-08-28 10:57:53 +0200

    Add super simple example that lists available serial ports

---
+++ example/example.dart
@@ -0,0 +1,15 @@
+import 'package:serial_port/serial_port.dart';
+
+void main() {
+  print('Available ports:');
+  var i = 0;
+  for (final name in SerialPort.availablePorts) {
+    final sp = SerialPort(name);
+    print('${++i}) $name');
+    print('\tDescription: ${sp.description}');
+    print('\tManufacturer: ${sp.manufacturer}');
+    print('\tSerial Number: ${sp.serialNumber}');
+    print('\tProduct ID: 0x${sp.productId.toRadixString(16)}');
+    print('\tVendor ID: 0x${sp.vendorId.toRadixString(16)}');
+  }
+}
