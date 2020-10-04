e2e9d6580b4c246ea47744118ce5691f2edd39d7
J-P Nurmi <jpnurmi@gmail.com>
2020-10-04 19:16:57 +0200

    Add some basic unit tests based on tty0tty

---
+++ test/dart_serial_port_test.dart
@@ -0,0 +1,73 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:dart_serial_port/dart_serial_port.dart';
+import 'package:test/test.dart';
+
+const EBUSY = 16; // Device or resource busy
+
+String get lastReason => SerialPort.lastError.message;
+Matcher get throwsError => throwsA(TypeMatcher<SerialPortError>());
+
+void main() {
+  setUp(() {
+    // https://github.com/lcgamboa/tty0tty
+    expect(Platform.isLinux, isTrue);
+    expect(File('/dev/tnt0').existsSync(), isTrue);
+    expect(File('/dev/tnt1').existsSync(), isTrue);
+  });
+
+  test('open & close', () {
+    final tnt0 = SerialPort('/dev/tnt0');
+
+    expect(tnt0.openRead(), isTrue, reason: lastReason);
+    expect(tnt0.close(), isTrue, reason: lastReason);
+
+    expect(tnt0.openWrite(), isTrue, reason: lastReason);
+    expect(tnt0.close(), isTrue, reason: lastReason);
+
+    expect(tnt0.openReadWrite(), isTrue, reason: lastReason);
+    expect(tnt0.close(), isTrue, reason: lastReason);
+  });
+
+  test('read & write', () {
+    final tnt0 = SerialPort('/dev/tnt0');
+    final tnt1 = SerialPort('/dev/tnt1');
+
+    expect(tnt0.openRead(), isTrue, reason: lastReason);
+    expect(tnt1.openWrite(), isTrue, reason: lastReason);
+
+    final bytes = Uint8List.fromList([1, 2, 3]);
+    expect(tnt1.write(bytes), bytes.length);
+    expect(tnt0.read(bytes.length), equals(bytes));
+
+    expect(tnt0.close(), isTrue, reason: lastReason);
+    expect(tnt1.close(), isTrue, reason: lastReason);
+  });
+
+  test('throws', () {
+    final tnt0 = SerialPort('/dev/tnt0');
+    final tnt1 = SerialPort('/dev/tnt1');
+
+    expect(tnt0.openRead(), isTrue, reason: lastReason);
+    expect(tnt1.openWrite(), isTrue, reason: lastReason);
+
+    final bytes = Uint8List.fromList([1, 2, 3]);
+    expect(() => tnt0.write(bytes), throwsError);
+    expect(() => tnt1.read(bytes.length), throwsError);
+
+    expect(tnt0.close(), isTrue, reason: lastReason);
+    expect(tnt1.close(), isTrue, reason: lastReason);
+  });
+
+  test('busy', () {
+    final tnt0 = SerialPort('/dev/tnt0');
+
+    expect(tnt0.openRead(), isTrue, reason: lastReason);
+
+    expect(tnt0.openRead(), isFalse, reason: lastReason);
+    expect(SerialPort.lastError.errorCode, equals(EBUSY));
+
+    tnt0.close();
+  });
+}
